57f4751ce0a8d69f2d217d74bbc3be2521806c3b
AyushKhamar <74979803+AyushKhamar@users.noreply.github.com>
2021-05-29 18:25:56 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,7 +10,17 @@ btn.addEventListener("click",()=>{
     let noq = parseInt(number.value);
     let ctg = cat.value;
     let lvl = level.value;
+    if(noq=="null"||ctg=="null"||lvl="null")
+    {
+        alert('Please select Data');
+        document.getElementById('temp').setAttribute('href','#');
+    }
+    else
+    {
+    
+
     localStorage.setItem("NumberOfQuestions",noq);
     localStorage.setItem("Catagory",ctg);
     localStorage.setItem("Level",lvl);
+    }
 })

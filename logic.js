ead822b28ab25aeba5489782a31c816aa5003093
Vrutik Prajapati <74986666+vrutik2809@users.noreply.github.com>
2021-05-30 01:08:05 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 //selector
 document.getElementById("baudio").setAttribute("src","background1.mp3");
 document.getElementById("baudio").volume = 0.7;
-console.log("Cicada");
+console.log("Cicadaa");
 var number = document.getElementById("numberofquestions");
 var cat = document.getElementById("category");
 var level = document.getElementById("difficulty");

bea70e707f9d1719e973b5cf4edf0588da37d92f
Vrutik Prajapati <74986666+vrutik2809@users.noreply.github.com>
2021-05-30 00:41:33 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 //selector
 document.getElementById("paudio").setAttribute("src","background.mp3");
-document.getElementById("paudio").volume = 0.2;
+document.getElementById("paudio").volume = 0.6;
 console.log("ayush khamar");
 var number = document.getElementById("numberofquestions");
 var cat = document.getElementById("category");

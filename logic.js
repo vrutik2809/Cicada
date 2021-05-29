201d8a941017c401b70a6eaac40d2e2803bd18bf
Vrutik Prajapati <74986666+vrutik2809@users.noreply.github.com>
2021-05-29 13:35:02 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,4 +1,14 @@
-
-
-let x = "ct sucks";
-console.log(x);
+//selector
+var number = document.getElementById("numberofquestions");
+var cat = document.getElementById("category");
+var level = document.getElementById("difficulty");
+var btn = document.getElementById("generate");
+//eventlistner
+btn.addEventListener("click",()=>{
+    let noq = parseInt(number.value);
+    let ctg = cat.value;
+    let lvl = level.value;
+    localStorage.setItem("NumberOfQuestions",noq);
+    localStorage.setItem("Catagory",ctg);
+    localStorage.setItem("Level",lvl);
+})
